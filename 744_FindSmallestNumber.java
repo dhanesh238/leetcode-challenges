70caeae7d643f61c0406797a1771fe980a064f57
Dhanesh Kumar <dhanesh238@users.noreply.github.com>
2023-06-09 22:56:14 +0530

    Create 744_FindSmallestNumber.java

---
+++ 744_FindSmallestNumber.java
@@ -0,0 +1,22 @@
+// 744. Find Smallest Letter Greater Than Target
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+/**
+* Input: letters = ["c","f","j"], target = "a"
+* Output: "c"
+* Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+* Input: letters = ["x","x","y","y"], target = "z"
+* Output: "x"
+* Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+*/
+class FindSmallestNumber {
+    public char nextGreatestLetter(char[] letters, char target) {
+        Arrays.sort(letters);
+        for(char eachLetter : letters) {
+            if(eachLetter > target) {
+                return eachLetter;
+            }
+        }
+        return letters[0];
+    }
+}
