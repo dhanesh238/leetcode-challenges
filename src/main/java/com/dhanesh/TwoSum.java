f1c29d06af677a1d5975ddd4469f1cb3bf2d74a0
Dhanesh Kumar <dhanesh238@gmail.com>
2024-06-22 11:44:10 +0530

    Optimized Time and Space complexity for 01. Two Sum

--- src/main/java/com/dhanesh/TwoSum.java
+++ src/main/java/com/dhanesh/TwoSum.java
@@ -1,9 +1,31 @@
 package com.dhanesh;
 
+import java.util.*;
+
 // 01. Two Sum
 // https://leetcode.com/problems/two-sum/description/
 public class TwoSum {
+
+    // Time O(n)
+    // Space O(n)
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            // Take eachNum check if (sum - eachNum) exists in HashMap
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            // Else add to HashMap
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        // Time O(n2)
         int[] result = new int[2];
         for (int i = 0; (i < nums.length - 1); i++) {
             for (int j = 1; (j < nums.length); j++) {
--- src/test/java/com/dhanesh/TwoSumTest.java
+++ src/test/java/com/dhanesh/TwoSumTest.java
@@ -11,12 +11,23 @@ class TwoSumTest {
     void twoSum() {
         assertAll(
                 () -> assertThat(twoSum.twoSum(new int[]{2,7,11,15}, 9))
-                        .isEqualTo(new int[]{0,1}),
+                        .containsExactly(new int[]{0,1}),
                 () -> assertThat(twoSum.twoSum(new int[]{3,2,4}, 6))
-                        .isEqualTo(new int[]{1,2}),
+                        .containsExactly(new int[]{1,2}),
                 () -> assertThat(twoSum.twoSum(new int[]{3,3}, 6))
-                        .isEqualTo(new int[]{0,1})
+                        .containsExactly(new int[]{0,1})
+        );
+    }
 
+    @Test
+    void twoSumBruteForce() {
+        assertAll(
+                () -> assertThat(twoSum.twoSumBruteForce(new int[]{2,7,11,15}, 9))
+                        .containsExactly(new int[]{0,1}),
+                () -> assertThat(twoSum.twoSumBruteForce(new int[]{3,2,4}, 6))
+                        .containsExactly(new int[]{1,2}),
+                () -> assertThat(twoSum.twoSumBruteForce(new int[]{3,3}, 6))
+                        .containsExactly(new int[]{0,1})
         );
     }
 }
